b803c1a48ce8bb6a3bf1e5dc0b373cde95839046
sazzadhossain1 <sazzadtomal1@gmail.com>
2023-06-22 05:04:28 +0600

    add carousel in home page

--- src/components/Header/Home/Home.js
+++ src/components/Header/Home/Home.js
@@ -3,7 +3,36 @@ import "./Home.css";
 const Home = () => {
   return (
     <div>
-      <h1>this is home</h1>
+      <div className="carousel w-full">
+        <div id="slide1" className="carousel-item relative w-full">
+          <img
+            src="https://onzo.progressionstudios.com/wp-content/uploads/2017/07/slide-1.jpg"
+            className="w-full"
+          />
+          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+            <a href="#slide4" className="btn btn-circle">
+              ❮
+            </a>
+            <a href="#slide2" className="btn btn-circle">
+              ❯
+            </a>
+          </div>
+        </div>
+        <div id="slide2" className="carousel-item relative w-full">
+          <img
+            src="https://onzo.progressionstudios.com/wp-content/uploads/2017/07/slide-2.jpg"
+            className="w-full"
+          />
+          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+            <a href="#slide1" className="btn btn-circle">
+              ❮
+            </a>
+            <a href="#slide3" className="btn btn-circle">
+              ❯
+            </a>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
