e80735a54c4f6bef3b858a0cd0780c73565b71fe
sazzadhossain1 <sazzadtomal1@gmail.com>
2023-06-23 02:26:20 +0600

    add three cart in home page with style

--- src/components/Discover/Discover.js
+++ src/components/Discover/Discover.js
@@ -1,12 +1,41 @@
 import React from "react";
 import "./Discover.css";
+import { Link } from "react-router-dom";
 
 const Discover = () => {
   return (
-    <div className="discover-parent-div">
-      <div>
-        <h3 className="easily">EASILY CUSTOMIZABLE</h3>
-        <h1 className="discover">Discover The Cycle ZzOon</h1>
+    <div>
+      <div className="discover-child-parent-div">
+        <div>
+          <h3 className="easily">EASILY CUSTOMIZABLE</h3>
+          <h1 className="discover">Discover The Cycle ZzOon</h1>
+        </div>
+      </div>
+      <div className="discover-grid-div">
+        <div className="common-div itsBorder">
+          <h3>
+            <Link>
+              <span className="number">01</span>
+              <span className="text">STARTER</span>
+            </Link>{" "}
+          </h3>
+        </div>
+        <div className="common-div itsBorder">
+          <h3>
+            <Link>
+              <span className="number">02</span>
+              <span className="text">ROAD</span>
+            </Link>
+          </h3>
+        </div>
+        <div className="common-div">
+          <h3>
+            <Link>
+              <span className="number">03</span>{" "}
+              <span className="text">PRO</span>
+            </Link>
+          </h3>
+        </div>
       </div>
     </div>
   );
