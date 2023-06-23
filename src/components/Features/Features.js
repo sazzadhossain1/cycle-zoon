b7d7df288982613ab441c5e2ed6744a8d0440cd2
sazzadhossain1 <sazzadtomal1@gmail.com>
2023-06-23 17:28:30 +0600

    add features component and add features component in home page

---
+++ src/components/Features/Features.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Features.css";
+
+const Features = () => {
+  return (
+    <div className="features-parent-div">
+      <h1>This is features</h1>
+    </div>
+  );
+};
+
+export default Features;
--- src/components/Header/Home/Home.js
+++ src/components/Header/Home/Home.js
@@ -2,11 +2,13 @@ import React from "react";
 import "./Home.css";
 import Carousel from "../../Carousel/Carousel";
 import Discover from "../../Discover/Discover";
+import Features from "../../Features/Features";
 const Home = () => {
   return (
     <div>
       <Carousel></Carousel>
       <Discover></Discover>
+      <Features></Features>
     </div>
   );
 };
