6032238f6282a29e8c06320de01a8fa5a0c5c3e3
sazzadhossain1 <sazzadtomal1@gmail.com>
2023-06-24 19:46:23 +0600

    add img in the gallery component and apply grid method

--- src/components/Gallery/Gellery.js
+++ src/components/Gallery/Gellery.js
@@ -8,6 +8,33 @@ const Gellery = () => {
       <h1 className="gallery-cycle">
         Cycle Zz<span className="gallery-oo">Oo</span>N in Action
       </h1>
+
+      <div className="gallery-grid-div">
+        <img
+          src="https://149357172.v2.pressablecdn.com/wp-content/uploads/2017/07/image-500x480.jpg"
+          alt=""
+        />
+        <img
+          src="https://149357172.v2.pressablecdn.com/wp-content/uploads/2017/07/image-6-500x480.jpg"
+          alt=""
+        />
+        <img
+          src="https://149357172.v2.pressablecdn.com/wp-content/uploads/2017/07/image-5-500x480.jpg"
+          alt=""
+        />
+        <img
+          src="https://149357172.v2.pressablecdn.com/wp-content/uploads/2017/07/image-3-500x480.jpg"
+          alt=""
+        />
+        <img
+          src="https://149357172.v2.pressablecdn.com/wp-content/uploads/2017/07/image-4-500x480.jpg"
+          alt=""
+        />
+        <img
+          src="https://149357172.v2.pressablecdn.com/wp-content/uploads/2017/07/image-tall-3-500x480.jpg"
+          alt=""
+        />
+      </div>
     </div>
   );
 };
