0577bc48d752a9f97611d6b907f8159b13a7c28a
sazzadhossain1 <sazzadtomal1@gmail.com>
2023-06-27 18:57:26 +0600

    add some photo in discover details page and apply grid method

--- src/components/DiscoverDetailsPage/DiscoverDetailsPage.js
+++ src/components/DiscoverDetailsPage/DiscoverDetailsPage.js
@@ -5,10 +5,39 @@ import { useLoaderData } from "react-router-dom";
 const DiscoverDetailsPage = () => {
   const singleData = useLoaderData();
   console.log(singleData);
+  const {
+    imgOne,
+    name,
+    nameTwo,
+    paragraph,
+    price,
+    smallImgFour,
+    smallImgOne,
+    smallImgTwo,
+    smallImgThree,
+  } = singleData;
   return (
     <div>
       <div className="discoverDetailsPage-backGround-Photo-div">
         <h2 className="shop">SHOP</h2>
+      </div>
+      <div className="discover-details-grid">
+        <div>
+          <img src={imgOne} alt="" />
+          <div>
+            <img src={smallImgOne} alt="" />
+            <img src={smallImgTwo} alt="" />
+            <img src={smallImgThree} alt="" />
+            <img src={smallImgFour} alt="" />
+          </div>
+        </div>
+        <div>
+          <h2 to="/home" className="discover-details-title-name   ">
+            Cycle Zz<span className="oo">Oo</span>N
+          </h2>
+          <p>{paragraph}</p>
+          <p>{price}</p>{" "}
+        </div>
       </div>
     </div>
   );
