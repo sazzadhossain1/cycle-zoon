86152263390557b16c8aee92bc7a3f133e68e62a
sazzadhossain1 <sazzadtomal1@gmail.com>
2023-06-29 20:09:04 +0600

    responsive done review section

--- src/components/CustomerReviews/CustomerReviews.js
+++ src/components/CustomerReviews/CustomerReviews.js
@@ -12,7 +12,7 @@ const CustomerReviews = ({ review }) => {
           src="https://149357172.v2.pressablecdn.com/wp-content/uploads/2017/07/star-rating-four-star.png"
           alt=""
         />
-        <p>{description}</p>
+        <p className="description">{description}</p>
         <div className="review-flex-div">
           <img className="review-img" src={img} alt="" />
           <div>
