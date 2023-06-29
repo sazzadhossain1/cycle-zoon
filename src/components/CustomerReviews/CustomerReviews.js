416ae4456c4d6df1afeb398689dd419e098bae5c
sazzadhossain1 <sazzadtomal1@gmail.com>
2023-06-29 18:59:13 +0600

    add description img name and customer

--- src/components/CustomerReviews/CustomerReviews.js
+++ src/components/CustomerReviews/CustomerReviews.js
@@ -3,7 +3,26 @@ import "./CustomerReviews.css";
 
 const CustomerReviews = ({ review }) => {
   console.log(review);
-  return <div></div>;
+  const { customer, description, img, name } = review;
+
+  return (
+    <div>
+      <div>
+        <img
+          src="https://149357172.v2.pressablecdn.com/wp-content/uploads/2017/07/star-rating-four-star.png"
+          alt=""
+        />
+        <p>{description}</p>
+        <div>
+          <img src={img} alt="" />
+          <div>
+            <h3>{name}</h3>
+            <p>{customer}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CustomerReviews;
