07b1d65541c42879bb48d1d27425f45b240e282c
sazzadhossain1 <sazzadtomal1@gmail.com>
2023-07-02 15:34:22 +0600

    create firebase file and install firebase packge

---
+++ src/firebase/firebase.init.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBdNee0fESk9YI4n8EuzOpwIwGJQkcuQj8",
+  authDomain: "cycle-zoon-two.firebaseapp.com",
+  projectId: "cycle-zoon-two",
+  storageBucket: "cycle-zoon-two.appspot.com",
+  messagingSenderId: "185898827364",
+  appId: "1:185898827364:web:c61bfef8800b86573b6971",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
