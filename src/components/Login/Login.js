bddad5caa6c8f25b29f0eee9dcfa07f025222710
sazzadhossain1 <sazzadtomal1@gmail.com>
2023-07-04 14:06:43 +0600

    create login from

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,8 +1,60 @@
 import React from "react";
 import "./Login.css";
+import { Link } from "react-router-dom";
 
 const Login = () => {
-  return <div></div>;
+  return (
+    <div className="signUp-parent-div ">
+      <div className="p-10">
+        <div className=" mx-auto signup-div p-5">
+          <h1 className="sign-head">SignUp</h1>
+          <h4 className="start-text">Get started with us today!</h4>
+          <form className="card-body  mx-auto">
+            <div className="form-control mx-auto">
+              <label className="label">
+                <span className="label-text">Your Email</span>
+              </label>
+              <label className="input-group">
+                <input
+                  type="email"
+                  name="email"
+                  placeholder="Enter Your Email"
+                  className=" input-field"
+                  required
+                />
+              </label>
+            </div>
+            <div className="form-control mx-auto">
+              <label className="label">
+                <span className="label-text">Password</span>
+              </label>
+              <label className="input-group">
+                <input
+                  type="password"
+                  name="password"
+                  placeholder="Password"
+                  className=" input-field"
+                  required
+                />
+              </label>
+            </div>
+
+            <h1 style={{ color: "white" }} className="m-5">
+              You new to this site?{" "}
+              <Link style={{ color: "blue" }} to="/signUp">
+                Please SignUp
+              </Link>
+            </h1>
+
+            <div className="  sign-up-btn ">
+              <button className="">SignUp</button>
+            </div>
+          </form>
+          <button className="  sign-up-btn ">SignIn With Google</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
