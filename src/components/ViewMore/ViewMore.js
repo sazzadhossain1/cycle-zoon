f92d63cd6f587f58cc1106d29106269996bb8fa9
sazzadhossain1 <sazzadtomal1@gmail.com>
2023-07-10 15:39:07 +0600

    add img with css in view more section style done start responsiveness

--- src/components/ViewMore/ViewMore.js
+++ src/components/ViewMore/ViewMore.js
@@ -3,45 +3,59 @@ import "./ViewMore.css";
 
 const ViewMore = () => {
   return (
-    <div>
+    <div className="view-main-parent-div">
       <div className="about-background-parent-div">
         <h1 className="about-title gallery">Gallery</h1>
       </div>
-      <div>
+      <div className="view-more-img-grid-div">
         <img
-          src="https://149357172.v2.pressablecdn.com/wp-content/uploads/2017/07/image-6-1024x768.jpg"
+          className="viewMore-img"
+          src="
+          
+          
+          https://149357172.v2.pressablecdn.com/wp-content/uploads/2017/07/image-tall-3.jpg"
           alt=""
         />
         <img
+          className="viewMore-img"
           src="https://149357172.v2.pressablecdn.com/wp-content/uploads/2017/07/image-5-1024x768.jpg"
           alt=""
         />
         <img
+          className="viewMore-img"
           src="https://149357172.v2.pressablecdn.com/wp-content/uploads/2017/07/image-4-1024x768.jpg"
           alt=""
         />
         <img
+          className="viewMore-img"
           src="https://149357172.v2.pressablecdn.com/wp-content/uploads/2017/07/image-tall-2.jpg"
           alt=""
         />
         <img
+          className="viewMore-img"
           src="https://149357172.v2.pressablecdn.com/wp-content/uploads/2017/07/image-tall-3.jpg"
           alt=""
         />
         <img
-          src="https://149357172.v2.pressablecdn.com/wp-content/uploads/2017/07/image-tall-3.jpg"
+          className="viewMore-img"
+          src="https://149357172.v2.pressablecdn.com/wp-content/uploads/2017/07/image-2-1024x768.jpg"
           alt=""
         />
         <img
+          className="viewMore-img"
           src="https://149357172.v2.pressablecdn.com/wp-content/uploads/2017/07/image-1024x768.jpg"
           alt=""
         />
         <img
+          className="viewMore-img"
           src="https://149357172.v2.pressablecdn.com/wp-content/uploads/2017/07/images-tall-1.jpg"
           alt=""
         />
         <img
-          src="https://149357172.v2.pressablecdn.com/wp-content/uploads/2017/07/image-2-1024x768.jpg"
+          className="viewMore-img"
+          src="
+          https://149357172.v2.pressablecdn.com/wp-content/uploads/2017/07/image-6-1024x768.jpg
+          "
           alt=""
         />
       </div>
