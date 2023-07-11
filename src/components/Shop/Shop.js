31b487122dd70021c9cd0597c259ae629322b403
sazzadhossain1 <sazzadtomal1@gmail.com>
2023-07-11 16:09:00 +0600

    add gackground img in shop component and create shop json fake api in publick component

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -4,9 +4,10 @@ import "./Shop.css";
 const Shop = () => {
   return (
     <div>
-      <h1>
-        <h1>Shop</h1>
-      </h1>
+      <div className="about-background-parent-div">
+        <h1 className="shop-shop">SHOP</h1>
+      </div>
+      <div></div>
     </div>
   );
 };
