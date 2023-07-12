6ba8c4da46309694ea65d6f1659fec2e351f213b
sazzadhossain1 <sazzadtomal1@gmail.com>
2023-07-12 16:09:42 +0600

    add input field from daisy ui

--- src/components/News/News.js
+++ src/components/News/News.js
@@ -8,7 +8,7 @@ const News = () => {
   // console.log(newsData);
   return (
     <div>
-      <div className="about-background-parent-div">
+      <div className="about-background-parent-div news-parent-div">
         <h1 className="shop-shop">News</h1>
       </div>
 
@@ -20,11 +20,36 @@ const News = () => {
         </div>
         {/*  */}
         <div>
-          <div>
+          <div className="news-search-div">
             <h3 className="search">Search</h3>
             <div className="news-blank-div"></div>
-            <input type="text" placeholder="Enter Keyword to search..." />
+            <div className="form-control">
+              <div className="input-group">
+                <input
+                  type="text"
+                  placeholder="Search…"
+                  className="input input-bordered"
+                />
+                <button className="btn btn-square">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-6 w-6"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                    />
+                  </svg>
+                </button>
+              </div>
+            </div>
           </div>
+          {/*  */}
           <div>
             <h2>RECENT NEWS</h2>
             <div></div>
