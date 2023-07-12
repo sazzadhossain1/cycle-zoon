d9ed9249e515cb2bb6598cac75482b1883a83d6f
sazzadhossain1 <sazzadtomal1@gmail.com>
2023-07-12 15:54:22 +0600

    add some contant and apply grid method

--- src/components/News/News.js
+++ src/components/News/News.js
@@ -12,13 +12,36 @@ const News = () => {
         <h1 className="shop-shop">News</h1>
       </div>
 
-      <div>
+      <div className="news-grid-div">
         <div>
           {newsData.map((data) => (
             <NewsDetail data={data} key={data.id}></NewsDetail>
           ))}
         </div>
-        <div></div>
+        {/*  */}
+        <div>
+          <div>
+            <h3 className="search">Search</h3>
+            <div className="news-blank-div"></div>
+            <input type="text" placeholder="Enter Keyword to search..." />
+          </div>
+          <div>
+            <h2>RECENT NEWS</h2>
+            <div></div>
+
+            <div>
+              <img
+                className="newsLogo-img"
+                src="https://w7.pngwing.com/pngs/669/871/png-transparent-document-book-page-line-book-text-paper-page.png"
+                alt=""
+              />
+              <div>
+                <p>DownHill: Taking Ride in Mountains</p>
+                <p>JUly 5, 2017</p>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
