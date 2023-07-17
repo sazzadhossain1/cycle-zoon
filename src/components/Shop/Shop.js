2bd8683b381e8c1dba638c9e6f326a6ed6190098
sazzadhossain1 <sazzadtomal1@gmail.com>
2023-07-17 13:22:47 +0600

    add text and img in shop component

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,5 +1,5 @@
 import "./Shop.css";
-import { useLoaderData } from "react-router-dom";
+import { Link, useLoaderData } from "react-router-dom";
 import ShopDetails from "../ShopDetails/ShopDetails";
 
 const Shop = () => {
@@ -10,10 +10,70 @@ const Shop = () => {
       <div className="about-background-parent-div">
         <h1 className="shop-shop">SHOP</h1>
       </div>
-      <div>
-        {shopData.map((data) => (
-          <ShopDetails data={data} key={data.id}></ShopDetails>
-        ))}
+      <div className="shop-parent-grid-div">
+        <div>
+          <img
+            src="https://149357172.v2.pressablecdn.com/wp-content/uploads/2017/07/onzo-elite.jpg"
+            alt=""
+          />
+          <div>
+            <h2>Cycle ZzOon</h2>
+            <div className="shop-blank-div"></div>
+            <p>$1,700</p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum
+              distinctio odit vitae inventore totam? Architecto, eligendi,
+              tempora sunt perspiciatis doloremque totam labore voluptatum rem
+              esse fuga possimus quod unde eum incidunt assumenda eaque quia
+              consequatur adipisci earum numquam reiciendis ducimus!
+            </p>
+            <button>
+              <Link>Select Option</Link>
+            </button>
+          </div>
+        </div>
+        <div>
+          <div>
+            <h2>Cycle ZzOon</h2>
+            <div className="shop-blank-div"></div>
+            <p>$1,700</p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum
+              distinctio odit vitae inventore totam? Architecto, eligendi,
+              tempora sunt perspiciatis doloremque totam labore voluptatum rem
+              esse fuga possimus quod unde eum incidunt assumenda eaque quia
+              consequatur adipisci earum numquam reiciendis ducimus!
+            </p>
+            <button>
+              <Link>Select Option</Link>
+            </button>
+          </div>
+          <img
+            src="https://149357172.v2.pressablecdn.com/wp-content/uploads/2017/07/product-red.jpg"
+            alt=""
+          />
+        </div>
+        <div>
+          <img
+            src="https://149357172.v2.pressablecdn.com/wp-content/uploads/2017/07/productsdark.jpg"
+            alt=""
+          />
+          <div>
+            <h2>Cycle ZzOon</h2>
+            <div className="shop-blank-div"></div>
+            <p>$1,700</p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum
+              distinctio odit vitae inventore totam? Architecto, eligendi,
+              tempora sunt perspiciatis doloremque totam labore voluptatum rem
+              esse fuga possimus quod unde eum incidunt assumenda eaque quia
+              consequatur adipisci earum numquam reiciendis ducimus!
+            </p>
+            <button>
+              <Link>Select Option</Link>
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/components/ShopDetails/ShopDetails.js
+++ src/components/ShopDetails/ShopDetails.js
@@ -1,13 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ShopDetails.css";
 
 const ShopDetails = ({ data }) => {
+  const getElement = document.querySelector("#color");
+  console.log(getElement);
   //   console.log(data);
   const { img, price, description } = data;
 
   return (
     <div id="div">
-      <div className="background"></div>
+      <div id="color" className="background">
+        <img src={img} alt="" />
+      </div>
     </div>
   );
 };
