d6c8d43ab0ae7defc496dea0b2a460e3296f0a2b
sazzadhossain1 <sazzadtomal1@gmail.com>
2023-07-19 15:58:10 +0600

    apply grid method with style

--- src/components/AccessoriesDetails/AccessoriesDetails.js
+++ src/components/AccessoriesDetails/AccessoriesDetails.js
@@ -1,16 +1,25 @@
 import React from "react";
 import { useLoaderData } from "react-router-dom";
+import "./AccessoriesDetails.css";
 
 const AccessoriesDetails = () => {
   const getAccessoriesData = useLoaderData();
-  console.log(getAccessoriesData);
+  const { title, img, price, description } = getAccessoriesData;
 
+  console.log(title, img, price, description);
   return (
     <div>
       <div className="about-background-parent-div">
         <h1 className="shop-shop">SHOP</h1>
       </div>
-      <h1>This is AccessoriesDetails</h1>
+      <div className="accessories-grid-div">
+        <img src={img} alt="" />
+        <div className="accessories-text-div">
+          <h3 className="accessories-title">{title}</h3>
+          <p className="accessories-description">{description}</p>
+          <p className="accessories-price">{price}</p>
+        </div>
+      </div>
     </div>
   );
 };
