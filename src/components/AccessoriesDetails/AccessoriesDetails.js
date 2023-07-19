450da570a86c70de7e0bacdd9297888d43563c14
sazzadhossain1 <sazzadtomal1@gmail.com>
2023-07-19 15:39:03 +0600

    load api in accessories component

--- src/components/AccessoriesDetails/AccessoriesDetails.js
+++ src/components/AccessoriesDetails/AccessoriesDetails.js
@@ -1,8 +1,15 @@
 import React from "react";
+import { useLoaderData } from "react-router-dom";
 
 const AccessoriesDetails = () => {
+  const getAccessoriesData = useLoaderData();
+  console.log(getAccessoriesData);
+
   return (
     <div>
+      <div className="about-background-parent-div">
+        <h1 className="shop-shop">SHOP</h1>
+      </div>
       <h1>This is AccessoriesDetails</h1>
     </div>
   );
